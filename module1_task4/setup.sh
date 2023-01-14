1408e03ef73ebd7a278f7cfac4b27f22fcf70f09
lijobazar <lijobazar@hotmail.com>
2023-01-13 17:02:17 -0800

    Correction SETUP OK

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo.deb
-rm hugo.deb
+dpkg -i hugo.deb
+rm -r hugo.deb
+apt -get install -y make
 make build
