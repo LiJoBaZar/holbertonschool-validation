88712addd57d7d402eb4e5211380d6a3883b5c28
lijobazar <lijobazar@hotmail.com>
2023-01-20 15:38:16 -0800

    must be updated to NOT install golang anymore

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
-rm -rf /usr/local/go
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-sudo apt-get install ./install_hugo.deb
+#!/usr/bin/env bash
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+rm -f hugo_extended_0.109.0_linux-amd64.deb
